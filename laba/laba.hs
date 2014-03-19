5009b64114545a4a11887ae569166b327886e2eb
Johan Swetzén <jswetzen@gmail.com>
2014-03-19 15:03:08 +0100

    Adds a sequenced and parallel scanl1

---
+++ laba/laba.hs
@@ -0,0 +1,46 @@
+module LabA where
+
+import Control.Parallel
+
+-- This is the original code from Mary
+type Fan a = [a] -> [a]
+
+mkFan :: (a -> a -> a) -> Fan a
+mkFan op (i:is) = i:[op i k | k <- is]
+
+pplus :: Fan Int
+pplus = mkFan (+)
+
+skl :: Fan a -> [a] -> [a]
+skl _ [a] = [a]
+skl f as = init los ++ ros'
+  where
+    (las,ras) = splitAt (cnd2 (length as)) as
+    (los,ros) = (skl f las, skl f ras)
+    ros' = f (last los : ros)
+
+cnd2 :: Integral a => a -> a
+cnd2 n = n - n `div` 2 -- Ceiling of n/2
+
+--- Start of our own copy with modifications
+
+-- The original code
+sscanl1 :: (a -> a -> a) -> [a] -> [a]
+sscanl1 _ [a] = [a]
+sscanl1 f as = init los ++ ros'
+  where
+    (las,ras) = splitAt (cnd2 (length as)) as
+    (los,ros) = (skl ff las, skl ff ras)
+    ros' = ff (last los : ros)
+    ff = mkFan f
+
+-- Parallel with par and pseq
+ppscanl1 :: (a -> a -> a) -> [a] -> [a]
+ppscanl1 _ [a] = [a]
+ppscanl1 f as = par ros (pseq los (init los ++ ros'))
+  where
+    (las,ras) = splitAt (cnd2 (length as)) as
+    (los,ros) = (skl ff las, skl ff ras)
+    ros' = ff (last los : ros)
+    ff = mkFan f
+
