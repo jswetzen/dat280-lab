c7349a3a602412f76c90a4237e6e4352e6833d56
Johan Swetzén <jswetzen@gmail.com>
2014-03-20 14:16:08 +0100

    Uses a parallel fib example to show that we can actually run something in parallel

--- laba/laba.hs
+++ laba/laba.hs
@@ -6,16 +6,33 @@ import System.Random (randoms, mkStdGen)
 import Control.Monad.Par
 
 main :: IO ()
+main = print $ runPar $ pfib 36
+-- main = print $ dcscanl1 slowOp $ take 500 $ repeat (2000::Int)
+-- main = print $ scanl1 slowOp $ take 500 $ repeat (1000::Int)
+-- main = print $ ppscanl1 slowOp $ take 500 $ repeat (1000::Int)
+-- main = print $ sscanl1 slowOp $ take 500 $ repeat (1000::Int)
+{-
 main = defaultMain
   [bench "scanl1" (nf (scanl1 slowOp) randomInts),
    bench "fscanl1" (nf (fscanl1 slowOp) randomInts),
    bench "sscanl1" (nf (sscanl1 slowOp) randomInts),
-   bench "ppscanl1" (nf (ppscanl1 slowOp) randomInts)]
+   bench "ppscanl1" (nf (ppscanl1 slowOp) randomInts),
+   bench "dcscanl1" (nf (dcscanl1 slowOp) randomInts)]
+ -}
 
-randomInts :: [Integer]
+pfib :: Int -> Par Int
+pfib n | n <= 2    = return 1
+       | otherwise = do
+          x <- spawn $ pfib (n-1)
+          y <- spawn $ pfib (n-2)
+          x' <- get x
+          y' <- get y
+          return (x' + y')
+
+randomInts :: [Int]
 randomInts =
-  take 2000 (randoms (mkStdGen 13402149018394))
-    :: [Integer]
+  take 200 (randoms (mkStdGen 13402149018394))
+    :: [Int]
 
 -- This is the original code from Mary
 type Fan a = [a] -> [a]
@@ -40,8 +57,9 @@ cnd2 n = n - n `div` 2 -- Ceiling of n/2
 --- Start of our own copy with modifications
 
 -- Slow operator
-slowOp :: Integer -> Integer -> Integer
-slowOp a b = a*b
+slowOp :: Integral a => a -> a -> a
+slowOp a b | a <= 0    = b
+           | otherwise = 1 + (slowOp (a-1) b)
 
 -- Fast scanl1
 fscanl1 :: (a -> a -> a) -> [a] -> [a]
@@ -76,8 +94,18 @@ ppscanl1' n f as = par ros (pseq los (init los ++ ros'))
     ff = mkFan f
 
 
---dcscanl1 :: (a -> a -> a) -> [a] -> [a]
---dcscanl1 f xs = divConq (\_ -> False
+dcscanl1 :: (NFData a) => (a -> a -> a) -> [a] -> [a]
+dcscanl1 f xs = divConq
+                  (\(i,_) -> i>=0)
+                  half
+                  combine
+                  solve
+                  (2,xs)
+  where
+    half (i,as) = toList i $ splitAt (cnd2 (length as)) as
+    toList i (las,ras) = [(i-1,las),(i-1,ras)]
+    combine (los:ros:_) = init los ++ (mkFan f) (last los : ros)
+    solve (_, as) = ppscanl1 f as
 
 divConq :: NFData sol
   => (prob -> Bool) -- indivisible?
