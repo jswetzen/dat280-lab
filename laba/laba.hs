96065da6315d57ed3559cd793e06c2f9271e6fd4
Johan Swetzén <jswetzen@gmail.com>
2014-03-20 12:50:42 +0100

    Adds divConq and a slower operator

--- laba/laba.hs
+++ laba/laba.hs
@@ -2,16 +2,19 @@ module Main where
 
 import Control.Parallel
 import Criterion.Main
-import System.Random
+import System.Random (randoms, mkStdGen)
+import Control.Monad.Par
 
 main :: IO ()
 main = defaultMain
-  [bench "sscanl1" (nf (sscanl1 (+)) randomInts),
-   bench "ppscanl1" (nf (ppscanl1 (+)) randomInts)]
+  [bench "scanl1" (nf (scanl1 slowOp) randomInts),
+   bench "fscanl1" (nf (fscanl1 slowOp) randomInts),
+   bench "sscanl1" (nf (sscanl1 slowOp) randomInts),
+   bench "ppscanl1" (nf (ppscanl1 slowOp) randomInts)]
 
 randomInts :: [Integer]
 randomInts =
-  take 200000 (randoms (mkStdGen 13402149018394))
+  take 2000 (randoms (mkStdGen 13402149018394))
     :: [Integer]
 
 -- This is the original code from Mary
@@ -36,23 +39,57 @@ cnd2 n = n - n `div` 2 -- Ceiling of n/2
 
 --- Start of our own copy with modifications
 
+-- Slow operator
+slowOp :: Integer -> Integer -> Integer
+slowOp a b = a*b
+
+-- Fast scanl1
+fscanl1 :: (a -> a -> a) -> [a] -> [a]
+fscanl1 _ []     = []
+fscanl1 _ [a]    = [a]
+fscanl1 f (a:b:as) = a : fscanl1 f (b':as)
+  where b' = f a b
+
 -- The original code
 sscanl1 :: (a -> a -> a) -> [a] -> [a]
 sscanl1 _ [a] = [a]
 sscanl1 f as = init los ++ ros'
   where
     (las,ras) = splitAt (cnd2 (length as)) as
-    (los,ros) = (skl ff las, skl ff ras)
+    (los,ros) = (sscanl1 f las, sscanl1 f ras)
     ros' = ff (last los : ros)
     ff = mkFan f
 
 -- Parallel with par and pseq
 ppscanl1 :: (a -> a -> a) -> [a] -> [a]
 ppscanl1 _ [a] = [a]
-ppscanl1 f as = par ros (pseq los (init los ++ ros'))
+ppscanl1 f as = ppscanl1' 5 f as
+
+ppscanl1' :: Int -> (a -> a -> a) -> [a] -> [a]
+ppscanl1' _ _ [a] = [a]
+ppscanl1' 0 f as = sscanl1 f as
+ppscanl1' n f as = par ros (pseq los (init los ++ ros'))
   where
     (las,ras) = splitAt (cnd2 (length as)) as
-    (los,ros) = (skl ff las, skl ff ras)
+    (los,ros) = (ppscanl1' (n-1) f las, ppscanl1' (n-1) f ras)
     ros' = ff (last los : ros)
     ff = mkFan f
 
+
+--dcscanl1 :: (a -> a -> a) -> [a] -> [a]
+--dcscanl1 f xs = divConq (\_ -> False
+
+divConq :: NFData sol
+  => (prob -> Bool) -- indivisible?
+  -> (prob -> [prob]) -- split into subproblems
+  -> ([sol] -> sol) -- join solutions
+  -> (prob -> sol) -- solve a subproblem
+  -> (prob -> sol)
+divConq indiv split join f prob
+  = runPar $ go prob
+  where
+    go prob
+      | indiv prob = return (f prob)
+      | otherwise = do
+        sols <- parMapM go (split prob)
+        return (join sols)
