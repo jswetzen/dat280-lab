a03a604254c8ddc36790bc747099cfba16db650d
Sebastian Lagermann <barafinkel@gmail.com>
2014-03-31 18:14:54 +0200

    Removed a probably useless function call.

--- laba/LabA.hs
+++ laba/LabA.hs
@@ -112,7 +112,7 @@ ppscanl1 f as = ppscanl1' 5 f as
 ppscanl1' :: NFData a => Int -> (a -> a -> a) -> [a] -> [a]
 ppscanl1' _ _ [a] = [a]
 ppscanl1' 0 f as = sscanl1 f as
-ppscanl1' n f as = rnf ros `par` rnf los `pseq` init los ++ ros'
+ppscanl1' n f as = rnf ros `par` los `pseq` init los ++ ros'
   where
     (las,ras) = splitAt (cnd2 (length as)) as
     (los,ros) = (ppscanl1' (n-1) f las, ppscanl1' (n-1) f ras)
