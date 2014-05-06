be6b42325114fa7b96d673579ab522c609246ca9
Johan Swetzén <jswetzen@gmail.com>
2014-05-06 09:52:11 +0200

    Properly working sequential buySell

--- labc/labC08.hs
+++ labc/labC08.hs
@@ -4,19 +4,30 @@ import Data.Array.Repa as R
 main :: IO ()
 main = print "hii"
 
+data State = State { buy :: (Int, Int) -- (value, position)
+                   , sell :: (Int, Int)
+                   , mini :: (Int, Int)
+                   } deriving (Show)
+
 -- Applying buySell should produce (1,5,10)
 exampleProblem :: Array U DIM1 Int
 exampleProblem = fromListUnboxed (Z :. (8::Int)) [0,0,2,9,8,10,1,10]
 
 buySellSeq :: Array U DIM1 Int -> (Int, Int, Int)
-buySellSeq arr = buySellSeq' (x, 0) (x, 0) (x, 0) 1 lst
+buySellSeq arr = buySellSeq' state 1 lst
   where (x:lst) = toList arr
+        state   = State (x, 0) (x, 0) (x, 0)
 
--- (buy value, position) -> (sell val, pos) -> (minimum val, pos) -> [list] -> (bPos, sPos, profit)
-buySellSeq' :: (Int, Int) -> (Int, Int) -> (Int, Int) -> Int -> [Int] -> (Int, Int, Int)
-buySellSeq' (bVal, bPos) (sVal, sPos) (_, _) _ [] = (bPos, sPos, sVal - bVal)
-buySellSeq' (bVal, bPos) (sVal, sPos) (mVal, mPos) pos (x:xs)
-  | x > sVal = buySellSeq' (bVal, bPos) (x, pos) (mVal, mPos) (pos + 1) xs -- Update sVal
-  | (x-mVal) > (sVal-bVal) = buySellSeq' (mVal, mPos) (x, pos) (mVal, mPos) (pos+1) xs -- Update buy/sell
-  | x < mVal = buySellSeq' (bVal, bPos) (sVal, sPos) (x, pos) (pos+1) xs -- Update mVal
+buySellSeq' :: State -> Int -> [Int] -> (Int, Int, Int)
+buySellSeq' s _ [] = let (bVal, bPos) = buy s
+                         (sVal, sPos) = sell s
+                     in  (bPos, sPos, sVal - bVal)
+buySellSeq' s p (x:xs)
+  | (x-mVal) > (sVal-bVal) = buySellSeq' (s {buy=mini s, sell=(x,p)}) (p+1) xs
+  | x <= mVal = buySellSeq' (s {mini=(x,p)}) (p+1) xs
+  | otherwise = buySellSeq' s (p+1) xs
+    where
+      (mVal, _) = mini s
+      (sVal, _) = sell s
+      (bVal, _) = buy s
 
