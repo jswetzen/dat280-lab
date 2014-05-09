9b7e65f42a893677cebb8a56a69296e2c49cb205
Johan Swetzén <jswetzen@gmail.com>
2014-05-09 16:47:57 +0200

    No LLVM

--- labc/labC08.hs
+++ labc/labC08.hs
@@ -1,16 +1,29 @@
 {-# LANGUAGE TypeOperators #-}
 module Main where
+import System.Random
+import Data.List
 import Data.Array.Repa as R
+import Data.Array.Repa.Repr.Unboxed
 import Data.Maybe as M
+import System.Environment
 
 main :: IO ()
-main = print "hii ^^"
--- main = do
-  -- res <- foldAllP foldFun foldBase exampleProblem'
-  -- print res
+main = do
+  args <- getArgs
+  let seed = mkStdGen 12934871823479128347
+      len = 20000
+      rs = randomlist len seed
+      arr = arrTup len rs
+      lst = listTup rs
+  case args of
+    ["-par"] -> print $ buySellP $ delay arr
+    _        -> print $ buySellSeq'' lst
 
--- solutionTest :: Monad m => m (BuySell, Min, Current)
--- solutionTest = foldAllP foldFun foldBase exampleProblem'
+runParallel :: Bool
+runParallel = False
+
+randomlist :: Int -> StdGen -> [Int]
+randomlist n = take n . unfoldr (Just . random)
 
 data State = State { buy :: (Int, Int) -- (value, position)
                    , sell :: (Int, Int)
@@ -19,16 +32,20 @@ data State = State { buy :: (Int, Int) -- (value, position)
 
 exampleList :: [Int]
 exampleList = [0,0,2,9,8,10,1,10]
+
+exampleListTup :: [(Int, Int, Int)]
+exampleListTup = Prelude.zip3 ([0..]::[Int]) ([0..]::[Int]) exampleList
+
 -- Applying buySell should produce (1,5,10)
-exampleProblem :: Array U DIM1 Int
-exampleProblem = fromListUnboxed (Z :. (8::Int)) exampleList
+exampleProblem :: Array U (Z :. Int) (Int, Int, Int)
+exampleProblem = fromListUnboxed (Z :. (8::Int)) exampleListTup
 
--- [((buy_pos, sell_pos), (min_val, min_pos), (pos, profit))]
--- exIds :: [(BuySell, Min, Current)]
--- exIds = zip3 (zip (repeat 0) (repeat 0)) (zip (repeat 0) (repeat 0)) (zip [0..] exampleList)
+listTup :: [Int] -> [(Int, Int, Int)]
+listTup = Prelude.zip3 ixs ixs
+  where ixs = [0..]::[Int]
 
--- exampleProblem' :: Array U DIM1 (BuySell, Min, Current)
--- exampleProblem' = fromListUnboxed (Z :. (8::Int)) exIds
+arrTup :: Int -> [Int] -> Array U (Z :. Int) (Int, Int, Int)
+arrTup len = fromListUnboxed (Z :. (len::Int)) . listTup
 
 ----------------
 -- Sequential --
@@ -60,42 +77,9 @@ buySellSeq' s p (x:xs)
 -- Try Two --
 -------------
 
--- type BuySell = (Int, Int)
--- type Min = (Int, Int)
--- type Current = (Int, Int) -- current
-
--- -- [((buy_pos, sell_pos), (min_val, min_pos), (pos, profit))]
--- exIdsFun :: [Int] -> [(BuySell, Min, Current)]
--- exIdsFun xs = zip3 (zip (repeat 0) (repeat 0)) (zip (repeat 0) (repeat 0)) (zip [0..] xs)
-
--- foldBase :: (BuySell, Min, Current)
--- foldBase = ((0, 0), (0, 0), (0, 0))
-
--- foldBaseArr :: Array U DIM1 (BuySell, Min, Current) -> (BuySell, Min, Current)
--- foldBaseArr arr = arr ! (Z :. 0)
-
--- foldFun :: (BuySell, Min, Current) ->
---            (BuySell, Min, Current) ->
---            (BuySell, Min, Current)
--- foldFun a1@(_, _, (pos, _))
---         a2@(_, _, (pos1, _)) =
---   if pos > pos1
---   then foldFunOrd a2 a1
---   else foldFunOrd a1 a2
-
--- foldFunOrd :: (BuySell, Min, Current) ->
---               (BuySell, Min, Current) ->
---               (BuySell, Min, Current)
--- foldFunOrd ((bp, sp), (mv, mp), (_, prof))
---         ((_, _), (_, _), (cpos, cval))
---   | (cval-mv) > prof = ((mp,cpos), (mv,mp), (cpos,cval-mv))
---   | cval <= mv = ((bp,sp), (cval,cpos), (cpos,prof))
---   | otherwise = ((bp,sp), (mv,mp), (cpos,prof))
-
 interleave :: [a] -> [a] -> [a]
 interleave [] xs = xs
 interleave (x:xs) ys = x:interleave ys xs
-
 
 odds :: [a] -> [a]
 odds (_:x:xs) = x:odds xs
@@ -115,16 +99,17 @@ scanOp op ident as = let
     s = scanOp op ident $ Prelude.zipWith op e o
   in interleave s $ Prelude.zipWith op s e
 
-buySellSeq'' :: [Int] -> Int
-buySellSeq'' as = maximum $ Prelude.zipWith (-) as minscan
+buySellSeq'' :: [(Int, Int, Int)] -> (Int, Int, Int)
+buySellSeq'' as = maximumS $ Prelude.zipWith minus as minscan
   where
-    minscan = scanOp min maxBound as
+    minscan = scanOp min' (head as) as
 
+maximumS :: [(Int, Int, Int)] -> (Int, Int, Int)
+maximumS as = foldr max' (head as) as
 
-pairwiseAdd :: Array U DIM1 Int -> Array D DIM1 Int
-pairwiseAdd arr = traverse arr halfsize indexfun
-  where halfsize (Z :. i) = Z :. i `div` 2
-        indexfun ixf (Z :. i) = ixf (ix1 (2*i)) + ixf (ix1 $ 2*i+1)
+--------------
+-- Parallel --
+--------------
 
 everyOther :: ((DIM1 -> a) -> DIM1 -> a) -> Array D DIM1 a -> Array D DIM1 a
 everyOther fun arr = traverse arr halfsize fun
@@ -136,7 +121,7 @@ evensFun ixf (Z :. i) = ixf (ix1 (2*i))
 oddsFun :: (DIM1 -> a) -> DIM1 -> a
 oddsFun ixf (Z :. i) = ixf (ix1 (2*i+1))
 
-scanOpP :: (a -> a -> a) -> a ->
+scanOpP :: (Unbox a) => (a -> a -> a) -> a ->
   Array D DIM1 a ->
   Array D DIM1 a
 scanOpP op ident as =
@@ -148,40 +133,34 @@ scanOpP op ident as =
     s = scanOpP op ident $ R.zipWith op e o
   in interleave2 s $ R.zipWith op s e
 
-buySellP :: Array D DIM1 Int -> Int
-buySellP arr = maximumP $ R.zipWith (-) arr minscan
+buySellP :: Array D DIM1 (Int, Int, Int) -> (Int, Int, Int)
+buySellP arr = maximumP $ R.zipWith minus arr minscan
   where
-    minscan = scanOpP min (arr ! (Z :. 0)) $ delay arr
+    minscan = scanOpP min' (arr ! (Z :. 0)) $ delay arr
+
+minus :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+minus (_, s1, v1) (b2, _, v2) = (b2, s1, v1-v2)
+
+min' :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+min' t1@(b1, _, p1) t2@(b2, _, p2)
+  | p1 < p2 = t1
+  | p1 > p2 = t2
+  | otherwise =
+    if b1 > b2
+    then t1
+    else t2
+
+max' :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+max' t1@(_, s1, p1) t2@(_, s2, p2)
+  | p1 > p2 = t1
+  | p1 < p2 = t2
+  | otherwise =
+    if s1 > s2
+    then t2
+    else t1
 
 -- fromJust is potentially unsafe, but should always give an answer
 -- And, we like to live dangerously
-maximumP :: Array D DIM1 Int -> Int
-maximumP arr = fromJust $ foldAllP max (arr ! (Z :. 0)) arr
+maximumP :: Array D DIM1 (Int, Int, Int) -> (Int, Int, Int)
+maximumP arr = fromJust $ foldAllP max' (arr ! (Z :. 0)) arr
 
-
-exampleList2 :: [Tuple]
-exampleList2 = zipTuple ([0..] :: [Int]) ([0..] :: [Int]) exampleList
-
-zipTuple :: [Int] -> [Int] -> [Int] -> [Tuple]
-zipTuple [] [] [] = []
-zipTuple (a:as) (b:bs) (c:cs) = T (a,b,c) : zipTuple as bs cs
-zipTuple _ _ _ = []
-
--- The tuple type is meant to describe the buy position, sell position and the
--- max profit outcome.
-newtype Tuple = T (Int, Int, Int) deriving Eq
-instance Ord Tuple where
-  T (_,_,c1) `compare` T (_,_,c2) = c1 `compare` c2
-  T (_,_,c1) >  T (_,_,c2) = c1 > c2
-  T (_,_,c1) >= T (_,_,c2) = c1 >= c2
-  T (_,_,c1) <  T (_,_,c2) = c1 < c2
-  T (_,_,c1) <= T (_,_,c2) = c1 <= c2
-  first@(T (_,_,c1)) `max` second@(T (_,_,c2)) = if c1 == max c1 c2
-                                                   then first
-                                                   else second
-  first@(T (_,_,c1)) `min` second@(T (_,_,c2)) = if c1 == min c1 c2
-                                                   then first
-                                                   else second
-
-instance Show Tuple where
-  showsPrec p (T x) = showsPrec p x
